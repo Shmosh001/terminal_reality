975ebb5b51421f81446902e90e43fda8a2a671eb
Andreas von Holy <vhland002@myuct.ac.za>
2015-06-30 14:46:51 +0200

    Character can cycle between animations

--- Terminal Reality/Assets/AnimationController.cs
+++ Terminal Reality/Assets/AnimationController.cs
@@ -3,15 +3,62 @@ using System.Collections;
 
 public class AnimationController : MonoBehaviour {
 
+	private string[] animations;
+	private string currentAnim;
+	private Animator anim;
+
+
+
+
+
+
 	// Use this for initialization
 	void Start () {
-	
+		animations = new string[]	{
+			"Run1",
+			"Run2",
+			"Run3",
+			"Walk1",
+			"Walk2",
+			"Wander"};
+		currentAnim = "Wander";
+		anim = this.gameObject.GetComponent<Animator>();
+		count = 0;
+		size = 5;
+
 	}
-	
+
+
+	private int count,size;
 	// Update is called once per frame
 	void Update () {
-		Animator anim = gameObject.GetComponent("Animator");
+
 		//anim = new Animation();
+		if(Input.GetKeyDown(KeyCode.Space)){
+			//currentAnim = "Run1";
+			//anim.Play(animations[3]);
+			count++;
+			if (count > 5){count = 0;}
+			currentAnim = animations[count];
+
+
+		
+		}
+
+
+
+	/*	if (Input.GetKeyDown(KeyCode.H)){
+			currentAnim = "Run2";
+
+		}*/
+		anim.speed = 1;
+		anim.Play(currentAnim);
+	}
+
+
+	void enterRunningAnimation(){}
+	void enterDyingAnimation(){}
+	void enterWanderingAnimation(){
 
 	}
 }
