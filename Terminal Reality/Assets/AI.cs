552bf2e50f634536f2e5b27ca1e8135ee1196cd1
Andreas von Holy <vhland002@myuct.ac.za>
2015-06-30 17:22:40 +0200

    Uncommited files for last commit

---
+++ Terminal Reality/Assets/AI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+
+	public Vector3 speed = new Vector3(0,0,-0.09f);
+
+
+
+	public FSM.State state;
+	public CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		state = FSM.State.stationary;
+		//controller.attachedRigidbody.
+		controller = (CharacterController)this.gameObject.GetComponent<CharacterController>();
+		//controller.Move(speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//we need to check the state of the enemy
+		controller.Move(speed);
+
+	}
+}
---
+++ Terminal Reality/Assets/FSM.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class FSM : MonoBehaviour {
+
+	public enum State{stationary,wandering,chase,search,attack,dead};
+	public State currentState;
+	public State prevState;
+
+
+	//need to maybe make an abstract class for this and then add implementation;
+	//void attack(){}=0;
+
+
+	//need element here that links to the actions for each state
+
+	abstract void enterStationary();
+
+	abstract void enterWandering();
+
+	public virtual abstract void enterChase();
+	abstract void enterSearch();
+	abstract void enterAttaack();
+	abstract void enterDead();
+
+
+	abstract void changeState(State newState);
+	abstract void previousState();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(currentState){
+		case State.attack:
+			break;
+		case State.dead:
+			break;
+		case State.chase:
+			break;
+		case State.search:
+			break;
+		case State.stationary:
+			break;
+		case State.wandering:
+			//need to execute the wandering code
+		
+			break;
+		
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
