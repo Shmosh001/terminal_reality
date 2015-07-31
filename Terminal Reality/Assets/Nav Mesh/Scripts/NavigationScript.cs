bf1434961730cdbaa45c75c34e250f4dfc6fb1e2
Andreas von Holy <andreas409@gmail.com>
2015-07-31 19:27:14 +0200

    basic path added and starting on implementation file

---
+++ Terminal Reality/Assets/Nav Mesh/Scripts/NavigationScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigationScript : MonoBehaviour {
+
+
+	public string pathName;
+	public string fileName;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		pathName = "Nav_Mesh/NAPS/Navigation Files";
+		fileName = "testpath";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void loadInPath(){
+		Pathfinding.LoadNavigationData(pathName, fileName);
+	}
+
+}
