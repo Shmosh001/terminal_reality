2a39a1836b7b22bf47bb4c7dcfd1c2a92988084b
Alon Bresler <abresler22@gmail.com>
2015-07-31 20:42:07 +0200

    Code written for reducing player's health.

--- Terminal Reality/Assets/Player/Player Scripts/playerHealthScript.cs
+++ Terminal Reality/Assets/Player/Player Scripts/playerHealthScript.cs
@@ -24,11 +24,28 @@ public class playerHealthScript : MonoBehaviour {
 	//REDUCE PLAYER'S HEALTH BY DAMAGE//
 	public void reducePlayerHealth(int damage)
 	{
+		//If the damage received does NOT kill the player
+		//i.e. damage does not make player health <= 0
+		if ((playerData.health - damage) > 0)
+		{
+			playerData.health -= damage;
+			updateHealthHUD();
+		}
+		//if the damage kills the player//
+		else if ((playerData.health - damage) <= 0)
+		{
+			playerData.health = 0;
+			print ("PLAYER IS DEAD!!!"); //temp print out
+		}
+
 	}
 
 	//INCREASE PLAYER'S HEALTH//
 	public void increasePlayerHealth(int healthPoints)
 	{
+
+		//TODO: IF WE DECIDE ON DOING INCREMENTAL HEALING AND NOT ONLY FULL HEALS//
+
 	}
 
 	//FULL UP (MAX) PLAYER'S HEALTH//
