a77de3f24dab78c4a1f6bfa0fd7150d9323fb605
Andreas von Holy <andreas409@gmail.com>
2015-08-02 00:18:12 +0200

    players position is updated every 1.5s with the unity nav mesh and AI updates path automatically

--- Terminal Reality/Assets/Nav Mesh/Scripts/UnityNavMesh.cs
+++ Terminal Reality/Assets/Nav Mesh/Scripts/UnityNavMesh.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class UnityNavMesh : MonoBehaviour {
 
 
+	private float counter1,time;
+
 	public Transform destination;
+	private NavMeshAgent agent;
 
 	// Use this for initialization
 	void Start () {
-		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent = GetComponent<NavMeshAgent>();
 		agent.destination = destination.position; 
+		time = 1.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		counter1 += Time.deltaTime;
+		if (counter1 > time){
+			agent.destination = destination.position; 
+			counter1 = 0;
+		}
 	}
 }
