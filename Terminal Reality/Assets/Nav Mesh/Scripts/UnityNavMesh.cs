6e86a85522cb55dbd1a8ceac1f55af5f52178144
Andreas von Holy <andreas409@gmail.com>
2015-08-02 00:15:06 +0200

    unity nav mesh implemented on a basic level

---
+++ Terminal Reality/Assets/Nav Mesh/Scripts/UnityNavMesh.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityNavMesh : MonoBehaviour {
+
+
+	public Transform destination;
+
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = destination.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
