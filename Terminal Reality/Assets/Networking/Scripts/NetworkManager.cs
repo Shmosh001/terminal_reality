8d83f6aed1e24c2d99a51fd8f4c9a835d13fb865
Andreas von Holy <andreas409@gmail.com>
2015-08-02 20:40:48 +0200

    player object spawns when we join a room

--- Terminal Reality/Assets/Networking/Scripts/NetworkManager.cs
+++ Terminal Reality/Assets/Networking/Scripts/NetworkManager.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class NetworkManager : MonoBehaviour {
 
 
-
+	public Camera mainCam;
 
 
 	// Use this for initialization
@@ -14,11 +14,32 @@ public class NetworkManager : MonoBehaviour {
 
 	void Connect(){
 		PhotonNetwork.ConnectUsingSettings("v1.0");
-
+		//PhotonNetwork.offlineMode = true;  we use this for single player
 	}
 
 	void OnGUI(){
 		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby(){
+		Debug.Log("OnJoinedLobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log("OnPhotonRandomJoinFailed");
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnJoinedRoom(){
+		Debug.Log("OnJoinedRoom");
+		SpawnPlayer();
+	}
+
+	void SpawnPlayer(){
+		Debug.Log("SpawnPlayer");
+		mainCam.enabled = false;
+		PhotonNetwork.Instantiate("First Person Controller", Vector3.zero, Quaternion.identity, 0);//group id is for seperating things
 	}
 
 
