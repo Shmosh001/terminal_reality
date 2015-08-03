6092991b4497983acbb1c4f2c8432a533b4da076
Andreas von Holy <andreas409@gmail.com>
2015-08-04 00:16:44 +0200

    added in player animations quickly to test aniamtion sync

--- Terminal Reality/Assets/Networking/Scripts/NetworkCharacter.cs
+++ Terminal Reality/Assets/Networking/Scripts/NetworkCharacter.cs
@@ -6,14 +6,14 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 
 	Vector3 foreignPosition;
 	Quaternion foreignRotation;
-
+	Animator animator;
 
 	public float smoothing = 0.1f;
 
 
 	// Use this for initialization
 	void Start () {
-	
+		animator = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -21,6 +21,7 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 		if (!photonView.isMine){
 			transform.position = Vector3.Lerp(transform.position, foreignPosition, smoothing);
 			transform.rotation = Quaternion.Lerp(transform.rotation, foreignRotation, smoothing);
+			//Mathf.Lerp(animator.GetFloat("Speed"), realspeed, 0.1f);//aniamtion lerping
 		}
 	}
 
@@ -30,11 +31,17 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 			//need to send position, meaning our player
 			stream.SendNext(transform.position);
 			stream.SendNext(transform.rotation);
+			//aniamtion sync
+			stream.SendNext(animator.GetFloat("Speed"));//provided we have speed
+			//sends the speed so that the aniamotr gets updated with this value
 		}
 		else{
 			//receiving other players things
 			foreignPosition = (Vector3)stream.ReceiveNext();
 			foreignRotation = (Quaternion)stream.ReceiveNext();
+			//receives speed vra for animation
+			animator.SetFloat("Speed",(float)stream.ReceiveNext());
+			//might need to add Mathf.lerp to lerp this
 		}
 	} 
 
