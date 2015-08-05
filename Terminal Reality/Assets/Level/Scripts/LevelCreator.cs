b979084a56a5c25c656a038b871a5b09282a2bd5
Andreas von Holy <vhland002@myuct.ac.za>
2015-08-05 11:25:22 +0200

    fixed level script and rebuilt

--- Terminal Reality/Assets/Level/Scripts/LevelCreator.cs
+++ Terminal Reality/Assets/Level/Scripts/LevelCreator.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 
 public class LevelCreator : MonoBehaviour {
+
+	public GameObject parent;
+
 	public GameObject wall;
 	public GameObject halfWall;
-
+	public bool deleteOld;
+	public bool showOld;
 	public GameObject[] oldWalls;
 	public GameObject[] oldHalfWalls;
 	// Use this for initialization
@@ -17,19 +21,46 @@ public class LevelCreator : MonoBehaviour {
 		//go through each wall object and replace with new wall
 		foreach (GameObject oldWall in oldWalls)
 		{
+
+
+			//Debug.LogError(oldWall.transform.rotation.y);
+
 			//positions dependant of rotation of wall
 			if(oldWall.transform.rotation.y == 0)
+			//if(oldWall.transform.rotation.y < 1 && oldWall.transform.rotation.y > -1)
 			{
+
 				GameObject tempWall = wall;
+				tempWall.transform.eulerAngles = new Vector3(tempWall.transform.eulerAngles.x, 0, tempWall.transform.eulerAngles.z);
 				tempWall.transform.rotation = new Quaternion(tempWall.transform.rotation.x,0,tempWall.transform.rotation.z,tempWall.transform.rotation.w);
 				Vector3 pos = new Vector3(oldWall.transform.position.x + 5.0f, oldWall.transform.position.y + 2.5f , oldWall.transform.position.z - 0.5f);
-				Instantiate(wall, pos, tempWall.transform.rotation);
+
+				GameObject obj = (GameObject)Instantiate(wall, pos, tempWall.transform.rotation);
+				//GameObject obj = (GameObject)Instantiate(wall, pos,  new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
+				obj.transform.parent = parent.transform;
+				if (!showOld){
+					oldWall.SetActive(false);
+				}
+				if (deleteOld){
+					Destroy(oldWall);
+				}
+
 			}
 
-			if(oldWall.transform.rotation.y == 90)
+			else if(oldWall.transform.eulerAngles.y > 90.0f )
 			{
+				GameObject tempWall = wall;
+				tempWall.transform.eulerAngles = new Vector3(tempWall.transform.eulerAngles.x, 90.0f, tempWall.transform.eulerAngles.z);
 				Vector3 pos = new Vector3(oldWall.transform.position.x - 0.5f, oldWall.transform.position.y + 2.5f , oldWall.transform.position.z - 5.0f);
-				Instantiate(wall, pos, wall.transform.rotation);
+				GameObject obj = (GameObject)Instantiate(wall, pos, tempWall.transform.rotation);
+				//GameObject obj = (GameObject)Instantiate(wall, pos, new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
+				obj.transform.parent = parent.transform;
+				if (!showOld){
+					oldWall.SetActive(false);
+				}
+				if (deleteOld){
+					Destroy(oldWall);
+				}
 			}
 		}
 
@@ -38,15 +69,32 @@ public class LevelCreator : MonoBehaviour {
 			if(oldHalfWall.transform.rotation.y == 0)
 			{
 				GameObject tempHalfWall = oldHalfWall;
+				tempHalfWall.transform.eulerAngles = new Vector3(90.0f, 0.0f, tempHalfWall.transform.eulerAngles.z);
 				tempHalfWall.transform.rotation = new Quaternion(tempHalfWall.transform.rotation.x,0,tempHalfWall.transform.rotation.z,tempHalfWall.transform.rotation.w);
 				Vector3 pos = new Vector3(oldHalfWall.transform.position.x + 2.5f, oldHalfWall.transform.position.y + 2.5f , oldHalfWall.transform.position.z - 0.5f);
-				Instantiate(halfWall, pos, tempHalfWall.transform.rotation);
+				GameObject obj = (GameObject)Instantiate(halfWall, pos, tempHalfWall.transform.rotation);
+				//GameObject obj = (GameObject)Instantiate(halfWall, pos,  new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
+				obj.transform.parent = parent.transform;
+				if (!showOld){
+					oldHalfWall.SetActive(false);
+				}
+				if (deleteOld){
+					Destroy(oldHalfWall);
+				}
 			}
 			
-			if(oldHalfWall.transform.rotation.y == 90)
+			if(oldHalfWall.transform.eulerAngles.y > 90.0f )
 			{
 				Vector3 pos = new Vector3(oldHalfWall.transform.position.x - 0.5f, oldHalfWall.transform.position.y + 2.5f , oldHalfWall.transform.position.z - 2.5f);
-				Instantiate(halfWall, pos, halfWall.transform.rotation);
+				GameObject obj = (GameObject)Instantiate(halfWall, pos, halfWall.transform.rotation);
+				//GameObject obj = (GameObject)Instantiate(halfWall, pos,  new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
+				obj.transform.parent = parent.transform;
+				if (!showOld){
+					oldHalfWall.SetActive(false);
+				}
+				if (deleteOld){
+					Destroy(oldHalfWall);
+				}
 			}
 		}
 	}
