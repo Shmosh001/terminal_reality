7c6189eac73d4d9553893ac183c3330837451349
Osher <oshwash@gmail.com>
2015-08-08 15:08:02 +0200

    padded rooms added. Completed another room and organized assets.

---
+++ Terminal Reality/Assets/Level/Scripts/LightFlicker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class LightFlicker : MonoBehaviour {
+
+		public float minIntensity = 0.0f; 
+		public float maxIntensity = 1.5f;		
+		float random;
+		
+		void Start()
+		{
+			//random = Random.Range(0.0f, 65535.0f);
+			random = 65535.0f;
+		}
+		
+		void Update()
+		{
+			float noise = Mathf.PerlinNoise(random, Time.time);
+			light.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+		}
+	
+}
