6228429e765ea66ed850499309832e060838f51e
Osher <oshwash@gmail.com>
2015-08-16 13:35:49 +0200

    New potential door added but needs to be animated to open and colse. New walls placed with proper texture.

--- Terminal Reality/Assets/Level/Scripts/LevelCreator.cs
+++ Terminal Reality/Assets/Level/Scripts/LevelCreator.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class LevelCreator : MonoBehaviour {
 
-	public GameObject parent;
+	//public GameObject parent;
 
 	public GameObject wall;
 	public GameObject halfWall;
@@ -15,8 +15,8 @@ public class LevelCreator : MonoBehaviour {
 	void Start () 
 	{
 		//fills lists with gameObjects of with the specified tag
-		oldWalls = GameObject.FindGameObjectsWithTag ("Old Wall");
-		oldHalfWalls = GameObject.FindGameObjectsWithTag ("Old Half Wall");
+		oldWalls = GameObject.FindGameObjectsWithTag ("Wall");
+		oldHalfWalls = GameObject.FindGameObjectsWithTag ("Half Wall");
 
 		//go through each wall object and replace with new wall
 		foreach (GameObject oldWall in oldWalls)
@@ -33,11 +33,11 @@ public class LevelCreator : MonoBehaviour {
 				GameObject tempWall = wall;
 				tempWall.transform.eulerAngles = new Vector3(tempWall.transform.eulerAngles.x, 0, tempWall.transform.eulerAngles.z);
 				tempWall.transform.rotation = new Quaternion(tempWall.transform.rotation.x,0,tempWall.transform.rotation.z,tempWall.transform.rotation.w);
-				Vector3 pos = new Vector3(oldWall.transform.position.x + 5.0f, oldWall.transform.position.y + 2.5f , oldWall.transform.position.z - 0.5f);
+				Vector3 pos = new Vector3(oldWall.transform.position.x, oldWall.transform.position.y, oldWall.transform.position.z);
 
 				GameObject obj = (GameObject)Instantiate(wall, pos, tempWall.transform.rotation);
 				//GameObject obj = (GameObject)Instantiate(wall, pos,  new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
-				obj.transform.parent = parent.transform;
+				//obj.transform.parent = parent.transform;
 				if (!showOld){
 					oldWall.SetActive(false);
 				}
@@ -47,14 +47,14 @@ public class LevelCreator : MonoBehaviour {
 
 			}
 
-			else if(oldWall.transform.eulerAngles.y > 90.0f )
+			else if(oldWall.transform.eulerAngles.y >= 90.0f )
 			{
 				GameObject tempWall = wall;
 				tempWall.transform.eulerAngles = new Vector3(tempWall.transform.eulerAngles.x, 90.0f, tempWall.transform.eulerAngles.z);
-				Vector3 pos = new Vector3(oldWall.transform.position.x - 0.5f, oldWall.transform.position.y + 2.5f , oldWall.transform.position.z - 5.0f);
+				Vector3 pos = new Vector3(oldWall.transform.position.x, oldWall.transform.position.y, oldWall.transform.position.z);
 				GameObject obj = (GameObject)Instantiate(wall, pos, tempWall.transform.rotation);
 				//GameObject obj = (GameObject)Instantiate(wall, pos, new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
-				obj.transform.parent = parent.transform;
+				//obj.transform.parent = parent.transform;
 				if (!showOld){
 					oldWall.SetActive(false);
 				}
@@ -71,10 +71,10 @@ public class LevelCreator : MonoBehaviour {
 				GameObject tempHalfWall = oldHalfWall;
 				tempHalfWall.transform.eulerAngles = new Vector3(90.0f, 0.0f, tempHalfWall.transform.eulerAngles.z);
 				tempHalfWall.transform.rotation = new Quaternion(tempHalfWall.transform.rotation.x,0,tempHalfWall.transform.rotation.z,tempHalfWall.transform.rotation.w);
-				Vector3 pos = new Vector3(oldHalfWall.transform.position.x + 2.5f, oldHalfWall.transform.position.y + 2.5f , oldHalfWall.transform.position.z - 0.5f);
+				Vector3 pos = new Vector3(oldHalfWall.transform.position.x, oldHalfWall.transform.position.y, oldHalfWall.transform.position.z);
 				GameObject obj = (GameObject)Instantiate(halfWall, pos, tempHalfWall.transform.rotation);
 				//GameObject obj = (GameObject)Instantiate(halfWall, pos,  new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
-				obj.transform.parent = parent.transform;
+				//obj.transform.parent = parent.transform;
 				if (!showOld){
 					oldHalfWall.SetActive(false);
 				}
@@ -83,12 +83,12 @@ public class LevelCreator : MonoBehaviour {
 				}
 			}
 			
-			if(oldHalfWall.transform.eulerAngles.y > 90.0f )
+			if(oldHalfWall.transform.eulerAngles.y >= 90.0f )
 			{
-				Vector3 pos = new Vector3(oldHalfWall.transform.position.x - 0.5f, oldHalfWall.transform.position.y + 2.5f , oldHalfWall.transform.position.z - 2.5f);
+				Vector3 pos = new Vector3(oldHalfWall.transform.position.x, oldHalfWall.transform.position.y, oldHalfWall.transform.position.z);
 				GameObject obj = (GameObject)Instantiate(halfWall, pos, halfWall.transform.rotation);
 				//GameObject obj = (GameObject)Instantiate(halfWall, pos,  new Quaternion (Quaternion.identity.x, 90, Quaternion.identity.z, Quaternion.identity.w));
-				obj.transform.parent = parent.transform;
+				//obj.transform.parent = parent.transform;
 				if (!showOld){
 					oldHalfWall.SetActive(false);
 				}
