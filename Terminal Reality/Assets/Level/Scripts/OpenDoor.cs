c47f7ddca4fffbe7806fd9b5bf96445a24a912b7
Andreas von Holy <andreas409@gmail.com>
2015-08-18 19:40:52 +0200

    redid door single door in blender and animated in unity, created animator controller for it and added in a basic script for controls

---
+++ Terminal Reality/Assets/Level/Scripts/OpenDoor.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+
+	private Animator animator;
+	private bool open;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator>();
+		open = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha1)){
+			openDoor ();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2)){
+			closeDoor ();
+		}
+	}
+
+	public void openDoor(){
+		if (!open){
+			animator.SetTrigger("Open");
+			open = true;
+		}
+	}
+
+
+	public void closeDoor(){
+		if (open){
+			animator.SetTrigger("Close");
+			open = false;
+		}
+	}
+}
