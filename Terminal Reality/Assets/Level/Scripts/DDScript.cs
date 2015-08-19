48449b051047b746fee7bb06a7fe01dc6fec86f8
Andreas von Holy <andreas409@gmail.com>
2015-08-19 18:12:33 +0200

    added in animations and aniamtion controllers for the double doors and added a script to control them

---
+++ Terminal Reality/Assets/Level/Scripts/DDScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DDScript : MonoBehaviour {
+
+
+	private bool open;
+	private Animator aniamtor;
+
+	// Use this for initialization
+	void Start () {
+		aniamtor = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Alpha6)){
+			openDoorForwards();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha7)){
+			closeDoorForwards();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha8)){
+			openDoorBackwards();
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha9)){
+			closeDoorBackwards();
+		}
+
+	}
+
+	public void openDoorForwards(){
+		if (!open){
+			aniamtor.SetTrigger("OpenFWD");
+		}
+	}
+
+
+	public void closeDoorForwards(){
+		if (open){
+			aniamtor.SetTrigger("CloseFWD");
+		}
+	}
+
+
+	public void openDoorBackwards(){
+		if (!open){
+			aniamtor.SetTrigger("OpenBCK");
+		}
+	}
+	
+	
+	public void closeDoorBackwards(){
+		if (open){
+			aniamtor.SetTrigger("CloseBCK");
+		}
+	}
+
+
+
+}
