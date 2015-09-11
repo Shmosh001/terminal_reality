fe1fa157c4c33c6a08b7bf7a886f82a242b38f54
Andreas von Holy <andreas409@gmail.com>
2015-09-11 19:37:08 +0200

    commented all networking code and deleted some broken meta files, ccaidentally deleted wheelchair model folder

--- Terminal Reality/Assets/Networking/Scripts/NetworkCharacter.cs
+++ Terminal Reality/Assets/Networking/Scripts/NetworkCharacter.cs
@@ -1,45 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// this class is assigned to a player and managed all the data that needs to be
+/// synced across the network
+/// it also receives all the data being synced
+/// </summary>
 public class NetworkCharacter : Photon.MonoBehaviour {
 
+    //PUBLIC VARS
+    public int sendRate = 20;
+    public int sendRateOnSerialize = 10;
+    public float lerpSmoothing = 5.0f;
 
-	private Vector3 foreignPosition;
+    //PRIVATE VARS
+
+    private Vector3 foreignPosition;
 	private Quaternion foreignRotation;
 	private Animator animator;
 
+    //other scripts that have data that needs to be synced
 	private playerDataScript playerData;
 	private torchScript torch;
 
+    //used to first set the script vars before syncing
 	private bool init = false;
 
-	//lerping smoothing factor
-	public float smoothing = 5.0f;
+	
 
 
-	// Use this for initialization
-	void Start () {
+    /// <summary>
+    /// initialization
+    /// </summary>
+    void Start () {
 		playerData = gameObject.GetComponent<playerDataScript>();
-		Debug.LogWarning(playerData);
 		torch = gameObject.GetComponentInChildren<torchScript>();
-		Debug.LogWarning(torch);
 		//animator = gameObject.GetComponent<Animator>();
-		//TODO should be fine
 		//PhotonNetwork.sendRate = 20;
 		//PhotonNetwork.sendRateOnSerialize = 10;
 		init = true;
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+    /// updates position if this photon view is ours (local player)
+    /// </summary>
 	void Update () {
-		if (!photonView.isMine && init){
-			transform.position = Vector3.Lerp(transform.position, foreignPosition, smoothing *  Time.deltaTime);
-			transform.rotation = Quaternion.Lerp(transform.rotation, foreignRotation, smoothing * Time.deltaTime);
-			//Mathf.Lerp(animator.GetFloat("Speed"), realspeed, 0.1f);//aniamtion lerping
+        //we lerp towards the received position to not teleport there immediately
+        if (!photonView.isMine && init){
+			transform.position = Vector3.Lerp(transform.position, foreignPosition, lerpSmoothing *  Time.deltaTime);
+			transform.rotation = Quaternion.Lerp(transform.rotation, foreignRotation, lerpSmoothing * Time.deltaTime);
+			//Mathf.Lerp(animator.GetFloat("Speed"), realspeed, 0.1f);//animation lerping
 		}
 	}
 
-
+    /// <summary>
+    /// gets called by photon and synces data
+    /// </summary>
+    /// <param name="stream">
+    /// stream of data
+    /// </param>
+    /// <param name="info">
+    /// info
+    /// </param>
 	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info){
 		Debug.Log("OnPhotonSerializeView");
 		//means we are sending local data
@@ -63,11 +85,19 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 				//might need to add Mathf.lerp to lerp this
 			}
 		}
-	} 
+	}
 
-	//Handles all the method calls to different scripts to send their data over the network
-	//NOTE: the order is important: needs to be the same as below in the receiving method
-	void sendData(PhotonStream stream, PhotonMessageInfo info){
+    /// <summary>
+    /// Handles all the method calls to different scripts to send their data over the network
+    /// NOTE: the order is important: needs to be the same as below in the receiving method
+    /// </summary>
+    /// <param name="stream">
+    /// stream of data
+    /// </param>
+    /// <param name="info">
+    /// info
+    /// </param>
+    void sendData(PhotonStream stream, PhotonMessageInfo info){
 		Debug.Log("sendData");
 		//POSITION AND ROTATION
 		stream.SendNext(transform.position);
@@ -80,9 +110,17 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 		torch.sendNetworkData(stream,info);
 	}
 
-	//handles all the received data that has been send over the network by calling the approriate methods in other scripts
-	//NOTE: the order is important: needs to be the same as above in the sending method
-	void receiveData(PhotonStream stream, PhotonMessageInfo info){
+    /// <summary>
+    /// handles all the received data that has been send over the network by calling the appropriate methods in other scripts
+    /// NOTE: the order is important: needs to be the same as above in the sending method
+    /// </summary>
+    /// <param name="stream">
+    /// stream of data
+    /// </param>
+    /// <param name="info">
+    /// info
+    /// </param>
+    void receiveData(PhotonStream stream, PhotonMessageInfo info){
 		Debug.Log("receiveData");
 		//POSITION AND ROTATION
 		foreignPosition = (Vector3)stream.ReceiveNext();
--- Terminal Reality/Assets/Networking/Scripts/NetworkManager.cs
+++ Terminal Reality/Assets/Networking/Scripts/NetworkManager.cs
@@ -1,73 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+/// <summary>
+/// joins a single client into the server and spawns their player character as well as enabling all the local methods
+/// </summary>
 public class NetworkManager : MonoBehaviour {
 
-	public bool offlineMode;
-	public GameObject mainCam;
-	private ObjectSpawner spawn;
-	public GameObject hud;
-	public GameObject temp;
 
-	// Use this for initialization
-	void Start () {
-		spawn = gameObject.GetComponent<ObjectSpawner>();
+    //PUBLIC VARS
+    public bool offlineMode;
+    //the hud
+    public GameObject hud;
+    //temp spawning location
+    public GameObject temp;
+    //main stand by camera 
+    public GameObject mainCam;
+
+
+    /// <summary>
+    /// initialization
+    /// </summary>
+    void Start () {
 		ConnectToNetwork(offlineMode);
 	}
 
+    /// <summary>
+    /// connects us to a network
+    /// </summary>
+    /// <param name="offline"></param>
 	void ConnectToNetwork(bool offline){
 		PhotonNetwork.ConnectUsingSettings("v1.0");
-		//PhotonNetwork.offlineMode = true;  we use this for single player
+		if (offline) {
+            PhotonNetwork.offlineMode = true; //we use this for single player
+        }
 		Debug.Log("ConnectToNetwork");
 	}
 
+    /// <summary>
+    /// gui events and update
+    /// </summary>
 	void OnGUI(){
+        //shows us the connection state top right corner
 		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
 	}
 
+    /// <summary>
+    /// when we join a lobby
+    /// </summary>
 	void OnJoinedLobby(){
 		Debug.Log("OnJoinedLobby");
 		PhotonNetwork.JoinRandomRoom();
 	}
 
+    /// <summary>
+    /// if random joining fails
+    /// create a room and join on that room
+    /// </summary>
 	void OnPhotonRandomJoinFailed(){
 		Debug.Log("OnPhotonRandomJoinFailed");
 		PhotonNetwork.CreateRoom(null);
 	}
 
+    /// <summary>
+    /// when we have joined a room
+    /// we spawn a player as he has successfully joined the server
+    /// </summary>
 	void OnJoinedRoom(){
 		Debug.Log("OnJoinedRoom");
 		SpawnPlayer();
 	}
 
-	//spawns a player into the game world using photons instantiate
-	void SpawnPlayer(){
+    /// <summary>
+    /// spawns a player into the game world using photons instantiate
+    /// </summary>
+    void SpawnPlayer(){
 		Debug.Log("SpawnPlayer");
 		mainCam.SetActive(false);
 		//PhotonNetwork.Instantiate("Player", spawn.position, spawn.rotation, 0);//group id is for seperating things
 
-
-//		Transform location = spawn.getSpawnLocation(ObjectSpawner.SpawnTypes.player);
 		Transform location = temp.transform;
 		//GameObject localPlayer = PhotonNetwork.Instantiate("First Person Controller", location.position, location.rotation, 0);//group id is for seperating things
 		GameObject localPlayer = PhotonNetwork.Instantiate("NEWPLAYER", location.position, location.rotation, 0);
 		//we enable all parts here that have to do with each local player ie movement, and mouse scripts and main camera
 		enableComponents(localPlayer);
 
-
-
 	}
 
-	//enables all components which have sensitive information local to a player
-	void enableComponents(GameObject localPlayer){
+    /// <summary>
+    /// enables all components which have sensitive information local to a player
+    /// </summary>
+    /// <param name="localPlayer">
+    /// player
+    /// </param>
+    void enableComponents(GameObject localPlayer){
 		Debug.Log("enableComponents");
 		localPlayer.GetComponent<PlayerMovementScript>().enabled = true;//enable the movement script
 		localPlayer.GetComponent<CharacterController>().enabled = true;
 		localPlayer.transform.FindChild("Main Camera").gameObject.SetActive(true);//enable camera again
-
-
-
-		localPlayer.GetComponent<ShootingScript>().enabled = true;//enable the shooting script
+        localPlayer.GetComponent<ShootingScript>().enabled = true;//enable the shooting script
 		localPlayer.GetComponent<interactionScript>().enabled = true;//enable the interaction script
 		localPlayer.GetComponent<playerDataScript>().enabled = true;//enable the data script
 		localPlayer.GetComponent<playerHealthScript>().enabled = true;//enable the health script
