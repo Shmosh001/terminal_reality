ce7769e4450c838740742d0da965e20a26db6851
Alon Bresler <abresler22@gmail.com>
2015-09-12 11:20:01 +0200

    Player can only sprint for 15 seconds straight. Then sprint energy needs to build up again. || Booleans for movement states added to playerDataScript.

--- Terminal Reality/Assets/Player/Player Scripts/playerDataScript.cs
+++ Terminal Reality/Assets/Player/Player Scripts/playerDataScript.cs
@@ -13,6 +13,11 @@ public class playerDataScript : MonoBehaviour {
 	public float mouseSpeed = 3.0f;
 	public float jumpSpeed = 5.5f;
 	public float pushingPower = 2.5f;
+
+	//PLAYER MOVEMENT STATES//
+	public bool walking = true;
+	public bool sprinting = false;
+	public bool sneaking = false;
 	
 	//PUBLIC WEAPON RELATED VARIABLES//	
 	public bool pistolPickedUp = false;
