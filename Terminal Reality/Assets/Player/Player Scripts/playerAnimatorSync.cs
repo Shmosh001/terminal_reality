41a6b3a3b3cd49c8d58e4d117aa644b1fef72fe4
Andreas von Holy <vhland002@myuct.ac.za>
2015-09-23 02:36:55 +0200

    layer sync forced

---
+++ Terminal Reality/Assets/Player/Player Scripts/playerAnimatorSync.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerAnimatorSync : MonoBehaviour {
+
+	
+    void Start() {
+        PhotonAnimatorView anim = this.gameObject.GetComponent<PhotonAnimatorView>();
+        anim.SetLayerSynchronized(0, PhotonAnimatorView.SynchronizeType.Continuous);
+        anim.SetLayerSynchronized(1, PhotonAnimatorView.SynchronizeType.Continuous);
+    }
+
+
+}
