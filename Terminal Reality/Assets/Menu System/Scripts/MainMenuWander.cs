e96617fa511076e780eff06ce6cfe4d7552620f0
Andreas von Holy <andreas409@gmail.com>
2015-10-01 18:35:34 +0200

    stripped out mian menu map and added zombies and paths to wander on, made seperate anim controllers, still need to test

---
+++ Terminal Reality/Assets/Menu System/Scripts/MainMenuWander.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuWander : MonoBehaviour {
+
+
+    public Transform[] locations;
+    private NavMeshAgent navAgent;
+    private int index = 0;
+    private int max;
+
+
+	// Use this for initialization
+	void Start () {
+        navAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        max = locations.Length-1;
+        nextLocation();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (checkArrival()) {
+            navAgent.Stop();
+            nextLocation();
+        }
+	}
+
+
+
+    /// <summary>
+    /// checks if we have arrived at a point
+    /// </summary>
+    /// <returns>
+    /// true/false
+    /// </returns>
+    bool checkArrival() {
+        //check if a path is already pending
+        if (!navAgent.pathPending) {
+            //check if we are close enough to the destination
+            if (navAgent.remainingDistance <= navAgent.stoppingDistance) {
+                //check if we have no path and that our velocity is 0
+                if (!navAgent.hasPath || navAgent.velocity.sqrMagnitude == 0f) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    void nextLocation() {
+        if (index > max) {
+            index = 0;
+        }
+
+        navAgent.SetDestination(locations[index].position);
+        index++;
+
+    }
+
+    
+}
