09b620e4eabab7e44665038815b0a1e493990d10
Andreas von Holy <andreas409@gmail.com>
2015-10-05 23:33:07 +0200

    basic wheelchair zombie added in and basic follow/stop follow script added, used with F5/F6

--- Terminal Reality/Assets/AI/Scripts/Tags.cs
+++ Terminal Reality/Assets/AI/Scripts/Tags.cs
@@ -30,6 +30,7 @@ public class Tags : MonoBehaviour {
     public const string P2TORCH = "P2Torch";
     public const string DEBUGLIGHT = "DebugLight";
     public const string RAGDOLL = "Ragdoll";
+   
 
 
 
---
+++ Terminal Reality/Assets/AI/Scripts/WheelChairChase.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelChairChase : MonoBehaviour {
+
+
+    private NavMeshAgent agent;
+    private Animator main;
+    private Animator fw;
+    private Animator bw;
+    private bool chasing;
+
+    // Use this for initialization
+    void Start () {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        main = gameObject.GetComponent<Animator>();
+        fw = gameObject.GetComponentInChildren<Animator>();
+        bw = gameObject.GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.F5)) {
+
+            agent.SetDestination(GameObject.FindGameObjectWithTag(Tags.PLAYER1).transform.position);
+            main.SetBool("Moving", true);
+            fw.SetBool("Moving", true);
+            bw.SetBool("Moving", true);
+            chasing = true;
+
+        }
+
+
+
+        if (chasing) {
+            agent.SetDestination(GameObject.FindGameObjectWithTag(Tags.PLAYER1).transform.position);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.F6)) {
+            agent.Stop();
+            chasing = false;
+            main.SetBool("Moving", false);
+            fw.SetBool("Moving", false);
+            bw.SetBool("Moving", false);
+        }
+    }
+}
