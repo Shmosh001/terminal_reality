6a9cda2793906418f36a78484fa4e0481cec4ce6
Alon Bresler <abresler22@gmail.com>
2015-10-11 13:41:36 +0200

    The camera on the player now moves up and back when the player dies.

---
+++ Terminal Reality/Assets/Player/Player Scripts/cameraScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+
+	private Transform player;
+	private Vector3 relCameraPos;
+	private float relCameraPosMag;
+	private float relCameraPosMagup;
+	private Vector3 newPos;
+
+	// Use this for initialization
+	void Awake () {
+	
+		player = gameObject.transform.parent;
+		
+		relCameraPos = transform.position - player.position;
+		relCameraPosMag = relCameraPos.magnitude + 100.0f;
+		relCameraPosMagup = relCameraPos.magnitude + 50.0f;
+	
+	}
+	
+	
+	public void playerDeadCam()
+	{
+		print ("CHANGING CAMERA POSITION!");
+		Vector3 standardPos = player.position + relCameraPos;
+		Vector3 abovePos = player.position + Vector3.up * relCameraPosMagup + Vector3.back * relCameraPosMag;
+		transform.position = Vector3.Lerp(transform.position, abovePos, 1.5f * Time.deltaTime);
+		
+		
+	}
+}
