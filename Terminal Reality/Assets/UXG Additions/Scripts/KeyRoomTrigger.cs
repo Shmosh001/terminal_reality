69e6092aa29e239dbb5b76e6a8d8ffd275c3f9af
Andreas von Holy <andreas409@gmail.com>
2016-03-04 20:48:45 +0200

    worked in keyroom

---
+++ Terminal Reality/Assets/UXG Additions/Scripts/KeyRoomTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyRoomTrigger : MonoBehaviour {
+
+
+
+    public PlayerAudioManager audio;
+
+	// Use this for initialization
+	void Start () {
+	    if (audio == null) {
+            Debug.LogError("not assigned");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == Tags.PLAYER1) {
+            audio.keyRoomSwitch();
+        }
+    }
+    void OnTriggerExit(Collider other) {
+        if (other.tag == Tags.PLAYER1) {
+            audio.normalSwitch();
+        }
+    }
+
+}
---
+++ Terminal Reality/Assets/UXG Additions/Scripts/PlayerAudioManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAudioManager : MonoBehaviour {
+
+
+
+    public AudioSource normalBG;
+    public AudioSource keyRoomBG;
+
+
+	// Use this for initialization
+	void Start () {
+	    if (normalBG == null || keyRoomBG == null) {
+            Debug.LogError("Not assigned");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    public void keyRoomSwitch() {
+        //switch on
+        keyRoomBG.Play();
+        normalBG.Stop();
+
+    }
+
+    public void normalSwitch() {
+        normalBG.Play();
+        keyRoomBG.Stop();
+    }
+}
