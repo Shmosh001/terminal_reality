158788dc31a9b78b3596bca37bbd35dcdac749f7
Andreas von Holy <andreas409@gmail.com>
2016-03-04 22:29:14 +0200

    started working on door trick and changed the ivy model

---
+++ Terminal Reality/Assets/UXG Additions/Scripts/DoorTrick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrick : MonoBehaviour {
+
+
+
+    public BoxCollider boxcol;
+    
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    void OnTriggerEnter(Collider col) {
+        //if (col.)
+    }
+
+
+
+    void OnTriggerExit(Collider col) {
+
+    }
+
+}
