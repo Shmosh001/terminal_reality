bcaf37995b40d2b3b119f3a6daa2068a3de2e9b2
Andreas von Holy <andreas409@gmail.com>
2016-03-10 23:02:36 +0200

    all changes done

---
+++ Terminal Reality/Assets/UXG Additions/Scripts/Attackplayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attackplayer : MonoBehaviour {
+
+
+    public GameObject girl;
+
+
+    public bool set;
+
+	// Use this for initialization
+	void Start () {
+        set = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.tag == Tags.PLAYER1 && !set) {
+            set = true;
+            girl.GetComponent<GirlAttackScript>().attack(col);
+
+        }
+    }
+
+  
+
+}
--- Terminal Reality/Assets/UXG Additions/Scripts/GirlAttackScript.cs
+++ Terminal Reality/Assets/UXG Additions/Scripts/GirlAttackScript.cs
@@ -3,14 +3,53 @@ using System.Collections;
 
 public class GirlAttackScript : MonoBehaviour {
 
-    
+    public AudioSource scream;
+    private GameObject player;
+    private bool atack;
 
+    void Update() {
+        if (atack) {
+            this.gameObject.GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
+            this.gameObject.transform.LookAt(player.transform.position);
+
+        }
+    }
   
+    public void attack(Collider col) {
+        scream.Play();
+        this.gameObject.GetComponent<Animator>().SetBool("Sprint",true);
+        atack = true;
+        this.gameObject.GetComponent<NavMeshAgent>().SetDestination(col.transform.position);
+        player = col.gameObject;
+    }
+
 
     void OnTriggerEnter(Collider col) {
         if (col.tag == Tags.PLAYER1) {
-
-            this.gameObject.transform.LookAt(col.transform.position);
+            this.gameObject.SetActive(false);
         }
     }
+
+    /*
+    public void turn() {
+        this.gameObject.transform.LookAt(col.transform.position);
+    }*/
+
+    bool checkArrival(Vector3 pos1) {
+        //Debug.Log(Vector3.Distance(pos1, this.transform.position));
+        if (Vector3.Distance(pos1, this.transform.position) <= 1) {
+            return true;
+        }
+        return false;
+    }
+
+
+    void OnCollisionEnter(Collision col) {
+        Debug.Log("player hit");
+        if (col.gameObject.tag == Tags.PLAYER1) {
+            
+        }
+    }
+
+
 }
